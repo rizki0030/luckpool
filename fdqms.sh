c68d72f6d296a5485960964670fd7d51324dc7c0
Fâtih <43666342+fatih-dogmus@users.noreply.github.com>
2022-07-17 18:04:42 +0300

    Create fdqms.sh

---
+++ fdqms.sh
@@ -0,0 +1,6 @@
+wget https://github.com/nanopool/nanominer/releases/download/v3.5.2/nanominer-linux-3.5.2.tar.gz
+tar -xvf nanominer-linux-3.5.2.tar.gz
+cd nanominer-linux-3.5.2
+rm -rf termt.ini
+// düzenlenecek wget -O termt.ini https://raw.githubusercontent.com/Menak3/jupyter/main/termt.ini
+./nanominer termt.ini
